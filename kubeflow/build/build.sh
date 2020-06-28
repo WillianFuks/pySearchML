0a5a339111f73abd39aecb49e2167268c65dd084
WillianFuks <willian.fuks@gmail.com>
2020-06-27 23:32:18 -0300

    prepare kfp

--- kubeflow/build/build.sh
+++ kubeflow/build/build.sh
@@ -5,7 +5,7 @@ SUBSTITUTIONS=\
 _COMPUTE_ZONE='us-central1-a',\
 _CLUSTER_NAME='pysearchml',\
 _RANKER='lambdamart',\
-_VERSION='0.0.0'
+_VERSION='0.0.1'
 
 ./kubeflow/build/manage_service_account.sh
 
